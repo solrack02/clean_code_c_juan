1486d40d1ab51141d83a2479b84c1a40b6b4a313
Carlos A. <carlosvsk@gmail.com>
2025-03-25 15:46:28 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -6,6 +6,13 @@ import * as RN from 'react-native';
  const root = ReactDOM.createRoot(document.getElementById('root'))
  root.render(
    <React.StrictMode>
-     <div>Olá Mundo3!</div>
+     <div>Olá Mundo 4!</div>
    </React.StrictMode>
  )
+
+
+RN.AppRegistry.registerComponent('App', () => App);
+
+RN.AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
