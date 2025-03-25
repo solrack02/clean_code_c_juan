f4189bcbcb040de080edb8d43a58132bd5fc6f36
Carlos A. <carlosvsk@gmail.com>
2025-03-25 15:44:08 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 
  import React from 'react';
  import ReactDOM from 'react-dom/client';
-import * as RN from 'react-native';
  
  const root = ReactDOM.createRoot(document.getElementById('root'))
  root.render(
