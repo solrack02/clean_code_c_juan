0f6466d09c24e1cba9d518ed56cdba1907c41fc3
Carlos A. <carlosvsk@gmail.com>
2025-03-25 11:33:50 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,10 +1,11 @@
 
- import React from 'react'
- import ReactDOM from 'react-dom/client'
+ import React from 'react';
+ import ReactDOM from 'react-dom/client';
+import * as RN from 'react-native';
  
  const root = ReactDOM.createRoot(document.getElementById('root'))
  root.render(
    <React.StrictMode>
-     <div>Olá Mundo!</div>
+     <RN.Text>Olá Mundo!</RN.Text>
    </React.StrictMode>
  )
