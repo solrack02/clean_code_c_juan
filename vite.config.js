77f6ce6387e82f46f5c9b98ebf956b1572fb7193
Carlos A. <carlosvsk@gmail.com>
2025-04-03 15:38:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-
 import { defineConfig } from 'vite';
-            import react from '@vitejs/plugin-react';
-            import path from 'path';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-            export default defineConfig({
-              plugins: [react()],
-              resolve: {
-                alias: {
-                  'react-native': 'react-native-web'
-                },
-                extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-              },
-              server: {
-                hmr: true,
-                watch: {
-                  usePolling: true,
-                },
-              },
-            });
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web'
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  server: {
+    hmr: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
