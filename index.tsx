09035386c0208ccabb1c82b1885ba0995d7a29db
Carlos A. <carlosvsk@gmail.com>
2025-04-04 13:41:26 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@ import * as RN from 'react-native';
 import { Router } from './src';
 
 export default function App() {
-  return <><RN.Text>Tela:</RN.Text><Router /></>;
+  return <><RN.Text>Tela:</RN.Text></>;
 }
 
 RN.AppRegistry.registerComponent('App', () => App);
